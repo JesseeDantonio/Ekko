6696fa462243a2d0b0b478341cc0bf107bb7d581
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-05-10 13:38:57 +0200

    refactor: simplification de la hiérarchie des classes d'événements
    Renommage de plusieurs classes et ajustement de l'héritage de `FactionEvent` vers `Faction` afin de rationaliser la structure et d'assurer une cohérence dans les noms.

--- src/main/java/com/alexandeh/ekko/factions/events/player/PlayerLeaveFaction.java
+++ src/main/java/com/alexandeh/ekko/factions/events/player/PlayerLeaveFaction.java
@@ -1,6 +1,6 @@
 package com.alexandeh.ekko.factions.events.player;
 
-import com.alexandeh.ekko.factions.events.FactionEvent;
+import com.alexandeh.ekko.factions.events.Faction;
 import com.alexandeh.ekko.factions.type.PlayerFaction;
 import lombok.Getter;
 import org.bukkit.entity.Player;
@@ -11,12 +11,12 @@ import org.bukkit.entity.Player;
  * explicit permission from original author: Alexander Maxwell
  */
 @Getter
-public class PlayerDisbandFactionEvent extends FactionEvent {
+public class PlayerLeaveFaction extends Faction {
 
     private PlayerFaction faction;
     private Player player;
 
-    public PlayerDisbandFactionEvent(Player player, PlayerFaction faction) {
+    public PlayerLeaveFaction(Player player, PlayerFaction faction) {
         this.player = player;
         this.faction = faction;
     }
